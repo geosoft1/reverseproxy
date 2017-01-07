d4ebfd92747110d11a8f2d0b6bc2cd8ac89f3033
geosoft1 <geosoft1@gmail.com>
2017-01-07 20:57:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+reverseproxy
+====
+
+Simple [reverse proxy](https://en.wikipedia.org/wiki/Reverse_proxy) server. Useful for accessing web applications on various servers (or VMs) through a single domain.
+
+## How it works?
+
+Just complete the `conf.json` file and run the server. Example:
+
+     {
+         "ip":"",
+         "port":"8080",
+         "routes":{
+		     "/upload":"192.168.88.160:8080",
+     	     "/Downloads/":"192.168.88.164:8000",
+     	     "#":"the pattern / matches all paths not matched by other registered patterns",
+     	     "/":"192.168.88.161"
+         }
+     }
+
+## Configuration details
+
+     "ip":"",
+
+No ip mean `localhost` on hosting server. Is no need to change this.
+
+     "port":"8080",
+
+The server listening on this port. Remeber to forward the port `80` to this port if your connection pass through a router. No root right are required if you run on big ports (eg. `8080`).
+
+## Routes
+
+Routes has the folowing structure
+
+     "path":"target"
+
+The path is what you request and the target is what you get (eg. if your domain is `example.com` then `/` mean `example.com/` and `/upload` mean `example.com/upload`).
+
+`#` path mean a comment and is not added to routes. Put the text in target. `#something` don't mean a comment.
+
+The reverse proxy add your path to the target, so be prepared to handle this path. For example the folowing will get an error page.
+
+     "/upload":"google.com"
+
+Use `/` path for main site which have index page on `/`. Use sufixes for other web services which have the sufix as main page.
+
+Do not repeat the routes because the server will take always tha last route to a target.
---
+++ main.go
@@ -0,0 +1,83 @@
+// simple http reverse proxy
+// Copyright (C) 2017  geosoft1  geosoft1@gmail.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"path/filepath"
+)
+
+func NewReverseProxy(target string) *httputil.ReverseProxy {
+	return httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: "http",
+		Host:   target,
+	})
+}
+
+func Handle(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("request:", r.RemoteAddr, "want", r.RequestURI)
+		//Many webservers are configured to not serve pages if a request doesn’t appear from the same host.
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+		p.ServeHTTP(w, r)
+	}
+}
+
+var Config map[string]interface{}
+
+func main() {
+	log.Print("init logger")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		os.Exit(1)
+	}
+
+	log.Print("load configuration")
+	f, err := os.Open(pwd + "/conf.json")
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	if err := json.NewDecoder(f).Decode(&Config); err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	log.Print("init routes")
+	count := 1
+	for Path, Target := range Config["routes"].(map[string]interface{}) {
+		// avoid add comments as route
+		if Path != "#" {
+			http.HandleFunc(Path, Handle(NewReverseProxy(Target.(string))))
+			log.Printf("r%d %s > %s", count, Path, Target)
+			count++
+		}
+	}
+
+	Address := fmt.Sprintf("%s:%s", Config["ip"], Config["port"])
+	log.Print("start listening on " + Address)
+	http.ListenAndServe(Address, nil)
+}
